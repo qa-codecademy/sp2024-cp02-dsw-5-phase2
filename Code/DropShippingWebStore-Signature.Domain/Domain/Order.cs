338c86ed06e7265c2bba7f277b3884ac162e03b7
Kristijannm <119734900+Kristijannm@users.noreply.github.com>
2024-09-21 15:14:37 +0200

    database models created

--- Code/DropShippingWebStore-Signature.Domain/Domain/Order.cs
+++ Code/DropShippingWebStore-Signature.Domain/Domain/Order.cs
@@ -9,6 +9,7 @@ namespace DropShippingWebStore_Signature.Domain.Domain
     public class Order : BaseEntity
     {
         public int UserId { get; set; }
+        public User User { get; set; }
         public decimal TotalPrice { get; set; }
         public DateTime CreatedAt { get; set; }
         public string Status { get; set; }
--- Code/DropShippingWebStore-Signature.Domain/Domain/User.cs
+++ Code/DropShippingWebStore-Signature.Domain/Domain/User.cs
@@ -18,7 +18,7 @@ namespace DropShippingWebStore_Signature.Domain.Domain
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public int Phone { get; set; }
+        public string Phone { get; set; }
         public ICollection<Order> Orders { get; set; } = new List<Order>();
         public int CartId { get; set; }
         public Cart Cart { get; set; }
